5c1d103528e4a3f22c83a2724c8931bdb864add2
Min-star-prog <2013103491@khu.ac.kr>
2026-09-14 20:28:19 +0900

    Robot3D (#4)
    ㅇㅂㅈㅈㅂㄷㅇㄱㄹ

--- include/robot_types.h
+++ include/robot_types.h
@@ -8,26 +8,31 @@ typedef struct {
     float y;
     uint8_t valid;
 } Point2D;
+typedef struct {
+    float x;
+    float y;
+    float z;
+    uint8_t valid;
+} Robot3D;
 
 typedef struct {
-    Point2D shoulder;
-    Point2D elbow;
-    Point2D wrist;
-
     Point2D finger1;
     Point2D finger2;
+    Point2D elbow;
+    Point2D wrist;
+    Point2D shoulder_l;
+    Point2D shoulder_r;
 
     uint32_t frame_id;
     uint8_t valid;
 } HumanArm2D;
 
 typedef struct {
-    float wrist_x_mm;
-    float wrist_z_mm;
-
-    float hand_pitch_deg;
-
-    float gripper_norm;
+    Robot3D finger1;
+    Robot3D finger2;
+    Robot3D wrist;
+    Robot3D elbow;
+    Robot3D shoulder;
 
     uint8_t valid;
 } Target2D;
@@ -38,7 +43,6 @@ typedef struct {
     float shoulder_deg;
     float elbow_deg;
     float wrist_pitch_deg;
-
     float wrist_roll_deg;
 
     float gripper_norm;
