7398f693dafa881c3783da7ea6d9e9982e5a54c5
ron2011 <ronvs@naver.com>
2026-09-14 20:33:21 +0900

    add: interface code robot_types.h

--- include/robot_types.h
+++ include/robot_types.h
@@ -2,43 +2,52 @@
 #define ROBOT_TYPES_H
 
 #include <stdint.h>
-
+// pixel data
 typedef struct {
     float x;
     float y;
     uint8_t valid;
 } Point2D;
 
+// pixel to robot data
 typedef struct {
-    Point2D shoulder;
-    Point2D elbow;
-    Point2D wrist;
+    float x;
+    float y;
+    float z;
+    uint8_t valid;
+} Robot3D;
 
+// CNN -> 이민
+typedef struct {
     Point2D finger1;
     Point2D finger2;
+    Point2D elbow;
+    Point2D wrist;
+    Point2D shoulder_l;
+    Point2D shoulder_r;
 
     uint32_t frame_id;
     uint8_t valid;
 } HumanArm2D;
 
+// 이민 -> 지승배
 typedef struct {
-    float wrist_x_mm;
-    float wrist_z_mm;
-
-    float hand_pitch_deg;
-
-    float gripper_norm;
+    Robot3D finger1;
+    Robot3D finger2;
+    Robot3D wrist;
+    Robot3D elbow;
+    Robot3D shoulder;
 
     uint8_t valid;
 } Target2D;
 
+// 지승배 -> 이승열
 typedef struct {
     float base_deg;
 
     float shoulder_deg;
     float elbow_deg;
     float wrist_pitch_deg;
-
     float wrist_roll_deg;
 
     float gripper_norm;
